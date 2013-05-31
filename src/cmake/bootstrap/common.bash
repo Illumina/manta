cb14e236d85378f433027dd76e14deececbc6aa8
Chris Saunders <csaunders@illumina.com>
2013-05-31 15:18:08 -0700

    frist round of functioning source

---
+++ src/cmake/bootstrap/common.bash
@@ -0,0 +1,51 @@
+#
+# Manta
+# Copyright (c) 2013 Illumina, Inc.
+#
+# This software is provided under the terms and conditions of the
+# Illumina Open Source Software License 1.
+#
+# You should have received a copy of the Illumina Open Source
+# Software License 1 along with this program. If not, see
+# <https://github.com/downloads/sequencing/licenses/>.
+#
+
+################################################################################
+##
+## Definition of functions and variables common to all bootstrap scripts.
+##
+## author Come Raczy
+##
+################################################################################
+
+function common_options () {
+    TEMP=`getopt -n $SCRIPT -o fc -- "$@"`
+    if [ $? != 0 ] ; then echo $SCRIPT: invalid option  >&2; echo "Terminating..." >&2 ; exit 2 ; fi
+    eval set -- "$TEMP"
+    FORCE=
+    CLEAN=
+    while true ; do
+        case "$1" in
+            -f) FORCE=true ; shift ;;
+            -c) CLEAN=true ; shift ;;
+            --)              shift ; break ;;
+            *) echo "Internal error!" >&2; exit 2 ;;
+        esac
+    done
+}
+
+function common_create_source () {
+    if [[ ! -e $SOURCE_TARBALL ]] ; then
+        echo $SCRIPT: source tarball $SOURCE_TARBALL not found >&2
+        exit 2
+    fi  
+    echo Decompressing $SOURCE_TARBALL >&2
+    mkdir -p ${BUILD_DIR}
+    tar -C ${BUILD_DIR} -${TARBALL_COMPRESSION}xf $SOURCE_TARBALL
+    
+    if [[ ! -d $SOURCE_DIR ]] ; then
+        echo $SOURCE_DIR does not exist >&2
+        exit 2
+    fi
+}
+
---
+++ src/cmake/bootstrap/installCmake.bash
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+#
+# Manta
+# Copyright (c) 2013 Illumina, Inc.
+#
+# This software is provided under the terms and conditions of the
+# Illumina Open Source Software License 1.
+#
+# You should have received a copy of the Illumina Open Source
+# Software License 1 along with this program. If not, see
+# <https://github.com/downloads/sequencing/licenses/>.
+#
+
+################################################################################
+##
+## Installation script for cmake
+##
+## author Come Raczy
+##
+################################################################################
+
+set -o nounset
+set -o pipefail
+
+REDIST_DIR=$1
+INSTALL_DIR=$2
+if [[ $# -ge 3 ]] ; then PARALLEL=$3 ; else PARALLEL=1 ; fi
+
+script_dir="$(dirname "$0")"
+source $script_dir/common.bash
+
+BUILD_DIR=${INSTALL_DIR}/build
+BIN_DIR=${INSTALL_DIR}/bin
+LIB_DIR=${INSTALL_DIR}/lib
+INCLUDE_DIR=${INSTALL_DIR}/include
+
+CMAKE_MAJOR=2
+CMAKE_MINOR=8
+CMAKE_PATCH=9
+CMAKE_PATCH_MIN=4
+CMAKE_REQUIRED="$CMAKE_MAJOR.$CMAKE_MINOR.$CMAKE_PATCH_MIN"
+TARBALL_VERSION="$CMAKE_MAJOR.$CMAKE_MINOR.$CMAKE_PATCH"
+SCRIPT=`basename "$0"`
+SOURCE_TARBALL=${REDIST_DIR}/cmake-$TARBALL_VERSION.tar.bz2
+TARBALL_COMPRESSION=j
+SOURCE_DIR=${BUILD_DIR}/cmake-$TARBALL_VERSION
+CMAKE_DIR=cmake-$CMAKE_MAJOR.$CMAKE_MINOR
+
+common_options $@
+
+if [[ $CLEAN ]] ; then
+    echo removing $SOURCE_DIR >&2
+    rm -rf $SOURCE_DIR
+    rm -rf ${INSTALL_DIR}/{doc,share}/$CMAKE_DIR
+    rm -f ${BIN_DIR}/{ccmake,cmake,cpack,ctest}
+    rm -f ${INSTALL_DIR}/man/man1/{ccmake,cmake,cmakecommands,cmakecompat,cmakemodules,cmakeprops,cmakevars,cpack,ctest}.1
+    exit 0
+fi
+
+AVAILABLE_CMAKE_VERSION=`cmake --version 2> /dev/null`
+if [[ "${AVAILABLE_CMAKE_VERSION}" =~ ^cmake\ version\ ([0-9]+)\.([0-9]+)\.([0-9]+) && ! $FORCE ]] ; then
+    MAJOR=${BASH_REMATCH[1]}
+    MINOR=${BASH_REMATCH[2]}
+    PATCH=${BASH_REMATCH[3]}
+    if [[ "$MAJOR" -eq "$CMAKE_MAJOR" && ( "$MINOR" -gt "$CMAKE_MINOR" || "$MINOR" -eq "$CMAKE_MINOR" && "$PATCH" -ge "$CMAKE_PATCH_MIN"  ) ]] ; then
+        echo "${BASH_REMATCH[0]} (>= $CMAKE_REQUIRED) is already installed" >&2
+        echo nothing to be done >&2
+        exit 1
+    fi
+fi 
+
+OLD_CMAKE_VERSION=`${BIN_DIR}/cmake --version 2> /dev/null`;
+if [[ $OLD_CMAKE_VERSION == "cmake version $TARBALL_VERSION" && ! $FORCE ]] ; then
+    echo cmake version \"$TARBALL_VERSION\" is already installed at ${BIN_DIR}/cmake >&2
+    echo nothing to be done >&2
+    exit 0
+elif [[ $OLD_CMAKE_VERSION != "" ]] ; then
+    echo unable to install cmake version \"$TARBALL_VERSION\" in ${BIN_DIR} >&2 
+    echo cmake version \"$OLD_CMAKE_VERSION\" is in the way. >&2
+    echo Please use an empty location to build the product. >&2
+    exit 2
+fi 
+
+
+##
+## cleanup all existing source directory before proceeding
+##
+rm -rf $SOURCE_DIR
+
+common_create_source
+
+echo "Extracted cmake version $TARBALL_VERSION source code into $SOURCE_DIR" >&2
+echo "Installing cmake using: './bootstrap --prefix=\"${INSTALL_DIR}\" --parallel=\"$PARALLEL\" && make -j \"$PARALLEL\" && make install'" >&2
+cd $SOURCE_DIR && ./bootstrap --prefix="${INSTALL_DIR}" --parallel="$PARALLEL" && make -j "$PARALLEL" && make install
+
+if [ $? != 0 ] ; then echo "cmake: build failed: Terminating..." >&2 ; exit 2 ; fi
+
+echo "Cleaning up ${SOURCE_DIR}" >&2
+rm -rf ${SOURCE_DIR}
+
+echo CMake installed successfully >&2
