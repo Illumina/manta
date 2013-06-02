82ba0d5ee94834bdf1012fc677c452b831f99b30
Chris Saunders <csaunders@illumina.com>
2013-06-01 18:05:28 -0700

    new script hit by re-header

--- src/c++/run_cppcheck.bash
+++ src/c++/run_cppcheck.bash
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 #
-# if cppcheck is found, run it on all c++ and return an error for *any* warning message:
+# Manta
+# Copyright (c) 2013 Illumina, Inc.
 #
+# This software is provided under the terms and conditions of the
+# Illumina Open Source Software License 1.
+#
+# You should have received a copy of the Illumina Open Source
+# Software License 1 along with this program. If not, see
+# <https://github.com/downloads/sequencing/licenses/>.
+#
+
+# if cppcheck is found, run it on all c++ and return an error for *any* warning message:
 
 set -o nounset
 set -o pipefail
