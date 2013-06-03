51c81f97a8e27e310f6cebb45143b1664109dc40
Chris Saunders <csaunders@illumina.com>
2013-06-03 11:48:37 -0700

    include simple sig handler

---
+++ src/c++/lib/blt_util/sig_handler.cpp
@@ -0,0 +1,62 @@
+// -*- mode: c++; indent-tabs-mode: nil; -*-
+//
+// Copyright (c) 2009-2013 Illumina, Inc.
+//
+// This software is provided under the terms and conditions of the
+// Illumina Open Source Software License 1.
+//
+// You should have received a copy of the Illumina Open Source
+// Software License 1 along with this program. If not, see
+// <https://github.com/downloads/sequencing/licenses/>.
+//
+
+/// \author Chris Saunders
+///
+
+#include "blt_util/log.hh"
+#include "blt_util/sig_handler.hh"
+
+#include <cstdlib>
+#include <signal.h>
+
+#include <iostream>
+#include <string>
+
+
+std::string _progname;
+std::string _cmdline;
+
+
+
+void
+blt_sig_handler (int sig) {
+
+    switch (sig) {
+    case SIGTERM:
+        log_os << "ERROR: " << _progname << " received termination signal. cmdline: " << _cmdline << std::endl;
+        exit(EXIT_FAILURE);
+#ifndef _WIN32
+    case SIGINT:
+        log_os << "ERROR: " << _progname << " received interupt signal. cmdline: " << _cmdline << std::endl;
+        exit(EXIT_FAILURE);
+#endif
+    default:
+        log_os << "INFO: " << _progname << " received signal no: " << sig << std::endl;
+        break;
+    }
+}
+
+
+
+void
+initialize_blt_signals(const char* progname,
+                       const char* cmdline) {
+
+    _progname=progname;
+    _cmdline=cmdline;
+
+    signal(SIGTERM, blt_sig_handler);
+#ifndef _WIN32
+    signal(SIGINT, blt_sig_handler);
+#endif
+}
