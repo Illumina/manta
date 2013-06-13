60a7e4f2ce70733540d9b9353801332e0cadbdb8
Chris Saunders <csaunders@illumina.com>
2013-06-13 06:16:58 -0700

    add stream handler

---
+++ src/c++/lib/manta_common/input_stream_handler.cpp
@@ -0,0 +1,168 @@
+// -*- mode: c++; indent-tabs-mode: nil; -*-
+//
+// Copyright (c) 2009-2013 Illumina, Inc.
+//
+// This software is provided under the terms and conditions of the
+// Illumina Open Source Software License 1.
+//
+// You should have received a copy of the Illumina Open Source
+// Software License 1 along with this program. If not, see
+// <https://github.com/downloads/sequencing/licenses/>.
+//
+
+/// \file
+///
+/// \author Chris Saunders
+///
+/// note coding convention for all ranges '_pos fields' is:
+/// XXX_begin_pos is zero-indexed position at the begining of the range
+/// XXX_end_pos is zero-index position 1 step after the end of the range
+///
+
+
+#include "blt_util/blt_exception.hh"
+#include "blt_util/log.hh"
+#include "manta_common/sinput_stream_handler.hh"
+
+#include <cstdlib>
+
+#include <iostream>
+#include <sstream>
+
+
+
+static
+const char*
+input_type_label(const INPUT_TYPE::index_t i) {
+    using namespace INPUT_TYPE;
+
+    switch(i) {
+    case NONE   : return "NONE";
+    case READ   : return "READ";
+    default :
+        log_os << "ERROR: unrecognized event type.\n";
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+
+void
+input_stream_data::
+register_error(const char* label,
+               const int sample_no) const {
+    log_os << "ERROR: attempting to register " << label
+           << " with sample number: " << sample_no
+           << " more than once\n";
+    exit(EXIT_FAILURE);
+}
+
+
+
+
+input_stream_handler::
+input_stream_handler(
+        const input_stream_data& data)
+    : _data(data)
+    , _is_end(false)
+    , _is_head_pos(false)
+    , _head_pos(0)
+{
+    // initial loading for _stream_queue:
+    const unsigned rs(_data._reads.size());
+    for(unsigned i(0); i<rs; ++i) {
+        push_next(INPUT_TYPE::READ,_data._reads.get_key(i),i);
+    }
+}
+
+
+
+bool
+input_stream_handler::
+next() {
+    if(_is_end) return false;
+
+    while(true) {
+        if(_current.itype != INPUT_TYPE::NONE) {
+            // reload stream_queue with current type and sample_no;
+            push_next(_current.itype,_current.sample_no,_current._order);
+            _last=_current;
+        }
+
+        if(_stream_queue.empty()) {
+            _current=input_record_info();
+            _is_end=true;
+            return false;
+        }
+        bool is_usable(true);
+        _current=_stream_queue.top();
+        _stream_queue.pop();
+
+        if(_is_head_pos &&
+           (_current.pos < _head_pos)) {
+            if (_current.itype == INPUT_TYPE::READ) {
+                std::ostringstream oss;
+                oss << "ERROR: unexpected read order:\n"
+                    << "\tInput-record with pos/type/sample_no: "
+                    << (_current.pos+1) << "/" << input_type_label(_current.itype) << "/" << _current.sample_no
+                    << " follows pos/type/sample_no: "
+                    << (_last.pos+1) << "/" << input_type_label(_last.itype) << "/" << _current.sample_no << "\n";
+                throw blt_exception(oss.str().c_str());
+            } else {
+                std::ostringstream oss;
+                oss << "ERROR: unexpected input type: " << _current.itype << "\n";
+                throw blt_exception(oss.str().c_str());
+            }
+        }
+
+        if(_is_head_pos) {
+            _head_pos=std::max(_head_pos,_current.pos);
+        } else {
+            _is_head_pos=true;
+            _head_pos=_current.pos;
+        }
+
+        if(is_usable) break;
+    }
+    return true;
+}
+
+
+
+static
+void
+get_next_read_pos(bool& is_next_read,
+                  pos_t& next_read_pos,
+                  bam_streamer& read_stream) {
+
+    is_next_read=read_stream.next();
+    if(is_next_read) {
+        const bam_record& read_rec(*(read_stream.get_record_ptr()));
+        next_read_pos=(read_rec.pos()-1);
+    } else {
+        next_read_pos=0;
+    }
+}
+
+
+
+void
+input_stream_handler::
+push_next(const INPUT_TYPE::index_t itype,
+          const int sample_no,
+          const unsigned order) {
+
+    bool is_next(false);
+    pos_t next_pos;
+    if       (itype == INPUT_TYPE::READ) {
+        bam_streamer& read_stream(*(_data._reads.get_value(order)));
+        get_next_read_pos(is_next,next_pos,read_stream);
+    } else {
+        std::ostringstream oss;
+        oss << "ERROR: unexpected input type: " << itype << "\n";
+        throw blt_exception(oss.str().c_str());
+    }
+    if(! is_next) return;
+    _stream_queue.push(input_record_info(next_pos,itype,sample_no,order));
+}
+
---
+++ src/c++/lib/manta_common/input_stream_handler.hh
@@ -0,0 +1,145 @@
+// -*- mode: c++; indent-tabs-mode: nil; -*-
+//
+// Copyright (c) 2009-2013 Illumina, Inc.
+//
+// This software is provided under the terms and conditions of the
+// Illumina Open Source Software License 1.
+//
+// You should have received a copy of the Illumina Open Source
+// Software License 1 along with this program. If not, see
+// <https://github.com/downloads/sequencing/licenses/>.
+//
+
+
+///
+/// object which accepts as input bam (and formerly vcf/other chromosome ordered) files from multiple
+/// samples and merges them in order
+///
+/// \author Chris Saunders
+///
+
+
+#pragma once
+
+
+#include "blt_util/id_map.hh"
+#include "blt_util/bam_streamer.hh"
+
+#include <map>
+#include <queue>
+#include <utility>
+
+
+namespace INPUT_TYPE {
+enum index_t { NONE, READ };
+}
+
+struct input_stream_hander;
+
+/// all inputs to be merged are registered to
+/// this object first
+struct input_stream_data {
+
+    void
+    register_reads(bam_streamer& bs,
+                   const int sample_no = 0) {
+        if(_reads.test_key(sample_no)) register_error("reads",sample_no);
+        _reads.insert(sample_no,&bs);
+    }
+
+private:
+
+    void
+    register_error(const char* label,
+                   const int sample_no) const;
+
+
+/////////// data:
+    friend struct input_stream_handler;
+    typedef id_map<int,bam_streamer*> reads_t;
+
+    reads_t _reads;
+};
+
+
+
+struct input_record_info {
+
+    input_record_info(const pos_t p = 0,
+                      const INPUT_TYPE::index_t t = INPUT_TYPE::NONE,
+                      const int i = 0,
+                      const unsigned s = 0)
+        :  pos(p), itype(t), sample_no(i), _order(s) {}
+
+    // reverse logic implied by operator< such that the 'lower' values
+    // we'd like to see first will come up on top of the
+    // priority_queue
+    //
+    bool
+    operator<(const input_record_info& rhs) const {
+        if(pos > rhs.pos) return true;
+        if(pos == rhs.pos) {
+            if(itype < rhs.itype) return true;
+            if(itype==rhs.itype) {
+                if (sample_no > rhs.sample_no) return true;
+                if (sample_no == rhs.sample_no) {
+                    return (_order > rhs._order);
+                }
+            }
+        }
+        return false;
+    }
+
+    unsigned get_order() const { return _order; }
+
+    pos_t pos;
+    INPUT_TYPE::index_t itype;
+    int sample_no;
+
+private:
+    friend struct input_stream_handler;
+
+    // record the submission order:
+    unsigned _order;
+};
+
+
+
+// streams multiple bam (and vcf) files to present the data
+// in positional order (but with offsets for vcfs to
+// run ahead of the bam reads)
+//
+struct input_stream_handler {
+
+    input_stream_handler(
+            const input_stream_data& data);
+
+    bool next();
+
+    input_record_info
+    get_current() const { return _current; }
+
+    pos_t
+    get_head_pos() const { return _head_pos; }
+
+private:
+
+    void
+    push_next(const INPUT_TYPE::index_t itype,
+              const int sample_no,
+              const unsigned order);
+
+
+///////////////////////////////// data:
+    const input_stream_data& _data;
+
+    input_record_info _current;
+    input_record_info _last;
+
+    bool _is_end;
+
+    bool _is_head_pos;
+    pos_t _head_pos;
+
+    std::priority_queue<input_record_info> _stream_queue;
+};
