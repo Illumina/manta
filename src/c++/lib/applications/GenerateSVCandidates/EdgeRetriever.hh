319d2f39267340d37b41888a4c10e44fc298a247
Chris Saunders <csaunders@illumina.com>
2013-07-06 17:32:24 -0700

    cleanup comments

--- src/c++/lib/applications/GenerateSVCandidates/EdgeRetriever.hh
+++ src/c++/lib/applications/GenerateSVCandidates/EdgeRetriever.hh
@@ -23,13 +23,13 @@
 
 /// provide an iterator over edges in a set of SV locus graphs
 ///
-/// iteration is designed to allow parallization of the graph
-/// iteration by dividing iteration into a set of bins with similar
-/// total edge observation counts
+/// designed to allow parallelization of the graph processing by
+/// dividing iteration into a set of bins with similar total edge
+/// observation counts
 ///
 struct EdgeRetriever
 {
-    /// \param binCount Total number of parallel bins, must be 1 or greater
+    /// \param binCount total number of parallel bins, must be 1 or greater
     /// \param binIndex parallel bin id, must be less than binCount
     EdgeRetriever(
         const SVLocusSet& set,
