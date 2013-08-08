91edb5821f9a851acb58e34fd668fa2a32f4d5dc
Chris Saunders <csaunders@illumina.com>
2013-08-07 17:02:36 -0700

    MANTA-19 add new unit test for SV scanner

---
+++ src/c++/lib/manta/test/SVLocusScannerTest.cpp
@@ -0,0 +1,51 @@
+// -*- mode: c++; indent-tabs-mode: nil; -*-
+//
+// Manta
+// Copyright (c) 2013 Illumina, Inc.
+//
+// This software is provided under the terms and conditions of the
+// Illumina Open Source Software License 1.
+//
+// You should have received a copy of the Illumina Open Source
+// Software License 1 along with this program. If not, see
+// <https://github.com/downloads/sequencing/licenses/>.
+//
+
+///
+/// \author Chris Saunders
+///
+
+#include "boost/test/unit_test.hpp"
+
+#include "manta/SVLocusScanner.cpp"
+
+
+
+BOOST_AUTO_TEST_SUITE( test_SVLocusScanner )
+
+BOOST_AUTO_TEST_CASE( test_getSVCandidates )
+{
+
+    ReadScannerOptions opt;
+
+    ALIGNPATH::path_t inputPath;
+    cigar_to_apath("100M2000D100M",inputPath);
+
+    bam_record bamRead;
+    bam1_t* bamDataPtr(bamRead.get_data());
+    edit_bam_cigar(inputPath,*bamDataPtr);
+
+    SimpleAlignment align(bamRead);
+
+    std::vector<SVCandidate> candidates;
+
+    getSVCandidatesFromRead(opt,align,0,candidates);
+
+
+    BOOST_REQUIRE_EQUAL(candidates.size(),1);
+    BOOST_REQUIRE(candidates[0].bp1.interval.range.is_pos_intersect(100));
+    BOOST_REQUIRE(candidates[0].bp2.interval.range.is_pos_intersect(2100));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
