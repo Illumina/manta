64721833ded80953fc0b8d620e028ace24b86842
Ole Schulz-Trieglaff <oschulz-trieglaff@illumina.com>
2013-08-13 15:08:23 +0100

    MANTA-17 Adding function to get the reverse complement

--- src/c++/lib/blt_util/bam_seq.hh
+++ src/c++/lib/blt_util/bam_seq.hh
@@ -67,6 +67,30 @@ get_bam_seq_char(const uint8_t a)
     }
 }
 
+inline
+char
+get_bam_seq_complement_char(const uint8_t a)
+{
+
+    using namespace BAM_BASE;
+
+    switch (a)
+    {
+    case REF:
+        return '=';
+    case A:
+        return 'T';
+    case C:
+        return 'G';
+    case G:
+        return 'C';
+    case T:
+        return 'A';
+    default:
+        return 'N';
+    }
+}
+
 
 inline
 uint8_t
@@ -153,6 +177,12 @@ struct bam_seq : public bam_seq_base
         return get_bam_seq_char(get_code(i));
     }
 
+    char
+    get_complement_char(const pos_t i) const
+    {
+    	return get_bam_seq_complement_char(get_code(i));
+    }
+
     std::string
     get_string() const
     {
@@ -160,6 +190,18 @@ struct bam_seq : public bam_seq_base
     	for (unsigned i(0); i<_size;++i) {
     		s[i] = get_char(i);
     	}
+    	return s;
+    }
+
+    // returns the reverse complement
+    std::string
+    get_rc_string() const
+    {
+    	std::string s(_size,'N');
+    	for (unsigned i(0); i<_size;++i) {
+    		s[i] = get_complement_char(i);
+    	}
+    	reverse(s.begin(),s.end());
     	return s;
     }
 
