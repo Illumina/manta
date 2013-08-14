28e5384c145c6cee088315e5fad32ea29a8e2e7f
Chris Saunders <csaunders@illumina.com>
2013-08-14 10:14:04 -0700

    MANTA-17 add more detail to assembly test

--- src/c++/lib/assembly/test/SmallAssemblerTest.cpp
+++ src/c++/lib/assembly/test/SmallAssemblerTest.cpp
@@ -25,6 +25,8 @@ BOOST_AUTO_TEST_SUITE( test_SmallAssembler )
 
 BOOST_AUTO_TEST_CASE( test_SmallAssembler1 )
 {
+    // test simple assembly functions at a single word size:
+
     SmallAssemblerOptions assembleOpt;
 
     assembleOpt.minWordLength = 6;
@@ -47,6 +49,14 @@ BOOST_AUTO_TEST_CASE( test_SmallAssembler1 )
     
     BOOST_REQUIRE_EQUAL(contigs.size(),1u);
     BOOST_REQUIRE_EQUAL(contigs[0].seq,"GTGTATTACCTAGTAC");
+    for(unsigned i(0);i<4;++i)
+    {
+        BOOST_REQUIRE(readInfo[i].isUsed);
+        BOOST_REQUIRE_EQUAL(readInfo[i].contigId,0u);
+    }
+    BOOST_REQUIRE(! readInfo[5].isUsed);
+
+
 }
 
 
