8da940c03389509966c5b19215c64e96cfecd143
Chris Saunders <csaunders@illumina.com>
2013-09-15 00:17:57 -0700

    add fast check for non-ascii symbols in source

---
+++ scratch/check_for_nonascii_chars.bash
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o pipefail
+
+reltoabs() {
+    (cd $1; pwd -P)
+}
+
+scriptdir=$(dirname $0)
+basedir=$(reltoabs $scriptdir/..)
+
+
+if [ $# != 0 ]; then
+    cat <<EOF
+
+usage: $0
+
+check for non-ascii characters in any manta code. If found hte filename, line number and a visual highlight within each line will be displayed
+
+EOF
+    exit 2
+fi
+
+
+find_cmake_source() {
+    base_dir=$1
+    find $base_dir -type f \
+        -name "*.cmake" -or \
+        -name "CMakeLists.txt"
+}
+
+find_cxx_source() {
+    base_dir=$1
+    find $base_dir -type f \
+        -name "*.cpp" -or \
+        -name "*.c" -or \
+        -name "*.hh" -or \
+        -name "*.h"
+}
+
+find_script_source() {
+    base_dir=$1
+    find $base_dir -type f \
+        -name "*.bash" -or \
+        -name "*.sh" -or \
+        -name "configure" -or \
+        -name "*.py"
+}
+
+get_source() {
+    for f in $basedir/src/*; do
+        dir=$(basename $f)
+        if [ $dir == "submodule" ]; then continue; fi
+        find_cmake_source $f
+        find_cxx_source $f
+        find_script_source $f
+    done
+}
+
+for f in $(get_source); do
+    #echo "checking: $f"
+    grep --color='auto' -n -H -P "[\x80-\xFF]" $f
+done
