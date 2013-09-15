6804bbaaf3c1b242746ca7e21648be59d49e7e7a
Chris Saunders <csaunders@illumina.com>
2013-09-15 15:50:49 -0700

    Fix license URL in all code headers

--- src/c++/bin/EstimateSVLoci.cpp
+++ src/c++/bin/EstimateSVLoci.cpp
@@ -8,7 +8,7 @@
 //
 // You should have received a copy of the Illumina Open Source
 // Software License 1 along with this program. If not, see
-// <https://github.com/downloads/sequencing/licenses/>.
+// <https://github.com/sequencing/licenses/>
 //
 
 #include "applications/EstimateSVLoci/EstimateSVLoci.hh"
