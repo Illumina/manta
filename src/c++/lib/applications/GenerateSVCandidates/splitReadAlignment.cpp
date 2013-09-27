f25b3dbb7aff818781e25814e24bd211b9cb8297
Xiaoyu Chen <xchen@xchen-VirtualBox.(none)>
2013-09-27 13:50:12 -0700

    add splitReadAlignment class

---
+++ src/c++/lib/applications/GenerateSVCandidates/splitReadAlignment.cpp
@@ -0,0 +1,98 @@
+// -*- mode: c++; indent-tabs-mode: nil; -*-
+//
+// Manta
+// Copyright (c) 2013 Illumina, Inc.
+//
+// This software is provided under the terms and conditions of the
+// Illumina Open Source Software License 1.
+//
+// You should have received a copy of the Illumina Open Source
+// Software License 1 along with this program. If not, see
+// <https://github.com/sequencing/licenses/>
+//
+
+///
+/// \author Xiaoyu Chen
+///
+
+#include "splitReadAlignment.hh"
+
+unsigned
+splitReadAlignment::
+calculateAlignScore(
+		const std::string& querySeq,
+		const std::string& scanWindow)
+{
+	const unsigned leftSize = _alignment.get_leftSize();
+	const unsigned querySize = querySeq.size();
+	unsigned leftMismatches(0);
+	unsigned rightMismatches(0);
+
+	for (unsigned i = 0; i<= leftSize; i++)
+		if (querySeq[i] != scanWindow[i]) leftMismatches += 1;
+
+	for (unsigned i = leftSize+1; i<querySize; i++)
+		if (querySeq[i] != scanWindow[i]) rightMismatches += 1;
+
+	const unsigned score = leftMismatches+rightMismatches;
+
+	_alignment.set_mismatches(leftMismatches, rightMismatches);
+	_alignment.set_score(score);
+
+	return (score);
+}
+
+void
+splitReadAlignment::
+align(const std::string& querySeq,
+	  const unsigned readMapQ,
+	  const std::string& targetSeq,
+	  const unsigned bpOffset)
+{
+	SRAlignmentInfo bestAlignInfo;
+
+	const unsigned querySize = querySeq.size();
+	const unsigned targetSize = targetSeq.size();
+	// set the scanning start & end to make sure the candidate windows overlapping the breakpoint
+	const unsigned scanStart = std::max((unsigned)0, (bpOffset - querySize + 2));
+	const unsigned scanEnd = std::min((bpOffset-1), (targetSize - querySize));
+
+	for (unsigned i = scanStart; i<= scanEnd; i++)
+	{
+		const unsigned leftSize = bpOffset - i + 1;
+		const unsigned rightSize= querySize - leftSize;
+		_alignment.set_sizes(leftSize, rightSize);
+		_alignment.set_mapQ(readMapQ);
+
+		const std::string scanWindow = targetSeq.substr(i, querySize);
+		const unsigned score = calculateAlignScore(querySeq, scanWindow);
+
+		if (score > bestAlignInfo.get_alignScore())
+		{
+			bestAlignInfo.update_alignInfo(_alignment);
+		}
+	}
+
+	// filtering the alignment and set evidence
+	set_evidence();
+}
+
+
+void
+splitReadAlignment::
+set_evidence()
+{
+	_hasEvidence = false;
+	const unsigned leftSize = _alignment.get_leftSize();
+	const unsigned rightSize = _alignment.get_rightSize();
+	const unsigned bestScore = leftSize + rightSize;
+
+	// filters for a read being counted as evidence
+	if (((_alignment.get_leftMismatches()/(float)leftSize) >= 0.5) ||
+		((_alignment.get_rightMismatches()/(float)rightSize) >= 0.5) ||
+		((_alignment.get_alignScore()/(float)bestScore) < 0.75))
+	{
+		_hasEvidence = true;
+		_evidence = 2 * std::min(leftSize, rightSize) / (float)(leftSize + rightSize);
+	}
+}
---
+++ src/c++/lib/applications/GenerateSVCandidates/splitReadAlignment.hh
@@ -0,0 +1,145 @@
+// -*- mode: c++; indent-tabs-mode: nil; -*-
+//
+// Manta
+// Copyright (c) 2013 Illumina, Inc.
+//
+// This software is provided under the terms and conditions of the
+// Illumina Open Source Software License 1.
+//
+// You should have received a copy of the Illumina Open Source
+// Software License 1 along with this program. If not, see
+// <https://github.com/sequencing/licenses/>
+//
+
+///
+/// \author Xiaoyu Chen
+///
+
+#pragma once
+
+#include <string>
+
+struct SRAlignmentInfo
+{
+	SRAlignmentInfo():
+		_leftSize(0),
+		_rightSize(0),
+		_leftMismatches(0),
+		_rightMismatches(0),
+		_readMapQ(0),
+		_alignScore(0)
+	{}
+
+	unsigned get_leftSize() const
+	{
+		return _leftSize;
+	}
+
+	unsigned get_rightSize() const
+	{
+		return _rightSize;
+	}
+
+	unsigned get_leftMismatches() const
+	{
+		return _leftMismatches;
+	}
+
+	unsigned get_rightMismatches() const
+	{
+		return _rightMismatches;
+	}
+
+	unsigned get_alignScore()const
+	{
+		return _alignScore;
+	}
+
+	float get_mapQ() const
+	{
+		return _readMapQ;
+	}
+
+	void set_sizes(unsigned lfs, unsigned rfs)
+	{
+		_leftSize = lfs;
+		_rightSize = rfs;
+	}
+
+	void set_mismatches(unsigned lmm, unsigned rmm)
+	{
+		_leftMismatches = lmm;
+		_rightMismatches = rmm;
+	}
+
+	void set_mapQ(float rmq)
+	{
+		_readMapQ = rmq;
+	}
+
+	void set_score(unsigned asc)
+	{
+		_alignScore = asc;
+	}
+
+
+	void update_alignInfo(
+				const SRAlignmentInfo& info)
+		{
+			_leftSize = info._leftSize;
+			_rightSize = info._rightSize;
+			_leftMismatches = info._leftMismatches;
+			_rightMismatches = info._rightMismatches;
+			_readMapQ = info._readMapQ;
+			_alignScore = info._alignScore;
+		}
+
+private:
+	unsigned _leftSize;
+	unsigned _rightSize;
+	unsigned _leftMismatches;
+	unsigned _rightMismatches;
+	float _readMapQ;
+	unsigned _alignScore;
+
+};
+
+
+struct splitReadAlignment
+{
+    splitReadAlignment():
+    	_hasEvidence(false),
+    	_evidence(0)
+    {}
+
+	float get_evidence() const
+    {
+    	return _evidence;
+    }
+
+    bool has_evidence() const
+    {
+    	return _hasEvidence;
+    }
+
+    unsigned
+    calculateAlignScore(
+    		const std::string& querySeq,
+    		const std::string& scanWindow);
+
+    void
+    align(const std::string& querySeq,
+    	  const unsigned readMapQ,
+    	  const std::string& targetSeq,
+          const unsigned bpOffset);
+
+
+private:
+
+    void set_evidence();
+
+    SRAlignmentInfo _alignment;
+    bool _hasEvidence;
+    float _evidence;
+
+};
