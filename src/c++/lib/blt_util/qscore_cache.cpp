e15e80e85aea9542727483bd04e66a884bfbb6af
Chris Saunders <csaunders@illumina.com>
2013-10-18 07:36:35 -0700

    MANTA-20 clean up qscore init

--- src/c++/lib/blt_util/qscore_cache.cpp
+++ src/c++/lib/blt_util/qscore_cache.cpp
@@ -11,10 +11,10 @@
 // <https://github.com/sequencing/licenses/>
 //
 
-/// \file
-
+///
 /// \author Chris Saunders
 ///
+
 #include "blt_util/blt_exception.hh"
 #include "blt_util/log.hh"
 #include "blt_util/math_util.hh"
@@ -30,22 +30,13 @@
 qphred_cache::
 qphred_cache()
 {
-    for (int i(0); i<=MAX_QSCORE; ++i)
-    {
-        q2p[i] = phred_to_error_prob(static_cast<double>(i));
-    }
-    for (int i(0); i<=MAX_QSCORE; ++i)
-    {
-        q2lncompe[i] = log1p_switch(-q2p[i]);
-    }
     static const double q2lnp(-std::log(10.)/10.);
-    for (int i(0); i<=MAX_QSCORE; ++i)
-    {
-        q2lne[i] = static_cast<double>(i)*q2lnp;
-    }
 
     for (int i(0); i<=MAX_QSCORE; ++i)
     {
+        q2p[i] = phred_to_error_prob(static_cast<double>(i));
+        q2lncompe[i] = log1p_switch(-q2p[i]);
+        q2lne[i] = static_cast<double>(i)*q2lnp;
         for (int j(0); j<=MAX_MAP; ++j)
         {
             mappedq[j][i] = error_prob_to_qphred(phred_to_mapped_error_prob(i,j));
