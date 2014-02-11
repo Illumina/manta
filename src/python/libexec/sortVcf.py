5aa1a9f85e77b4f6b42b9c98e46efb15f8776a1f
Chris Saunders <csaunders@illumina.com>
2014-02-11 12:03:21 -0800

    Update source header to include 2014

--- src/python/libexec/sortVcf.py
+++ src/python/libexec/sortVcf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # Manta
-# Copyright (c) 2013 Illumina, Inc.
+# Copyright (c) 2013-2014 Illumina, Inc.
 #
 # This software is provided under the terms and conditions of the
 # Illumina Open Source Software License 1.
