fa02926e5809d8e6854b615d0fb5149d4df23d89
Xiaoyu Chen <xchen@xchen-VirtualBox.(none)>
2014-04-25 16:37:52 -0700

    debug

--- src/c++/lib/applications/GenerateSVCandidates/SVCandidateAssemblyRefiner.cpp
+++ src/c++/lib/applications/GenerateSVCandidates/SVCandidateAssemblyRefiner.cpp
@@ -30,7 +30,7 @@
 #include <iostream>
 
 //#define DEBUG_REFINER
-//#define DEBUG_CONTIG
+#define DEBUG_CONTIG
 #ifdef DEBUG_CONTIG
 static const std::string logtag("DEBUG CONTIG: ");
 #endif
@@ -476,15 +476,18 @@ isSmallSVAlignment(
     	const std::string rightContig = contigSeq.substr(endPos, rightSize);
 
     	const int searchWindow = 500;
-    	const float mismatchRate(0.05);
+    	//const float mismatchRate(0.05);
     	const int refAlignStart = align.beginPos;
     	const int refAlignEnd = align.beginPos + apath_ref_length(apath);
 
     	// search leftContig in the downstream of refStart
     	const int leftSearchStart = std::max(0, refAlignEnd-searchWindow);
     	const std::string refSeq4LeftSearch = refSeq.substr(leftSearchStart, (refAlignEnd-leftSearchStart));
-    	unsigned occurrences = searchContig(refSeq4LeftSearch, leftContig, mismatchRate);
+    	unsigned occurrences = 0;
+    	//unsigned occurrences = searchContig(refSeq4LeftSearch, leftContig, mismatchRate);
+
 #ifdef DEBUG_CONTIG
+    	log_os << "refSeq4LeftSearch" << refSeq4LeftSearch;
     	log_os << logtag << "left contig has size " << leftSize << ":\n" << leftContig << "\n";
     	log_os << logtag << "left contig occurrences " << occurrences << "\n";
 #endif
@@ -493,8 +496,9 @@ isSmallSVAlignment(
     	// search rightContig in the upstream of refEnd
     	const int rightSearchSize = std::min(searchWindow, int(refSeq.length()-refAlignStart));
     	const std::string refSeq4RightSearch = refSeq.substr(refAlignStart, rightSearchSize);
-    	occurrences = searchContig(refSeq4RightSearch, rightContig, mismatchRate);
+    	//occurrences = searchContig(refSeq4RightSearch, rightContig, mismatchRate);
 #ifdef DEBUG_CONTIG
+    	log_os << "refSeq4RightSearch" << refSeq4RightSearch;
     	log_os << logtag << "right contig has size " << rightSize << ":\n" << rightContig << "\n";
     	log_os << logtag << "right contig occurrences " << occurrences << "\n";
 #endif
