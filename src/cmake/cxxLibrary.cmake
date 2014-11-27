5f2b17908398c023bf6723220f56ed9f5aba9c0c
Chris Saunders <csaunders@illumina.com>
2014-11-27 12:21:45 -0800

    Fix pyflow install. Share starka build system
    A variable mismatch was causing the pyflow installation to be
    missed. In the process of fixing this more of the build system
    has been redesigned to be shared with starka.

--- src/cmake/cxxLibrary.cmake
+++ src/cmake/cxxLibrary.cmake
@@ -49,8 +49,8 @@ endforeach ()
 
 if (THIS_LIBRARY_SOURCES)
     #include_directories (${THIS_COMMON_INCLUDE})
-    add_library     (manta_${CURRENT_DIR_NAME} STATIC ${THIS_LIBRARY_SOURCES})
-    add_dependencies(manta_${CURRENT_DIR_NAME} MANTA_OPT)
+    add_library     (${THIS_PROJECT_NAME}_${CURRENT_DIR_NAME} STATIC ${THIS_LIBRARY_SOURCES})
+    add_dependencies(${THIS_PROJECT_NAME}_${CURRENT_DIR_NAME} THIS_OPT)
 endif()
 
 ##
