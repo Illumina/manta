d54ad6946f29110fc999663752d5e95aea99483c
Felix Schlesinger <fschlesinger@illumina.com>
2015-03-27 11:22:39 -0700

    RNA: Fix LocusScanner debug logging

--- src/c++/lib/manta/SVLocusScanner.cpp
+++ src/c++/lib/manta/SVLocusScanner.cpp
@@ -365,7 +365,7 @@ getSACandidatesFromRead(
     {
         candidates.push_back(GetSplitSACandidate(dopt, localRead, localAlign, ral, fragSource));
 #ifdef DEBUG_SCANNER
-        log_os << __FUNCTION__ << ": evaluating SA sv for inclusion: " << candidates.back() << "\n";
+        log_os << __FUNCTION__ << ": evaluating SA sv for inclusion: " << candidates.data.back() << "\n";
 #endif
     }
 }
