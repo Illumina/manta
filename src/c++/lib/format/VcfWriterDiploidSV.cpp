2e987f38f33eb2b6a272a6ed768ac74d41c1f887
Chris Saunders <csaunders@illumina.com>
2015-09-11 17:28:45 -0700

    MANTA-260 fix vcf header

--- src/c++/lib/format/VcfWriterDiploidSV.cpp
+++ src/c++/lib/format/VcfWriterDiploidSV.cpp
@@ -57,6 +57,7 @@ VcfWriterDiploidSV::
 addHeaderFormat() const
 {
     _os << "##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">\n";
+    _os << "##FORMAT=<ID=FT,Number=1,Type=String,Description=\"Sample filter, 'PASS' indicates that all filters have passed for this sample\">\n";
     _os << "##FORMAT=<ID=GQ,Number=1,Type=Float,Description=\"Genotype Quality\">\n";
     _os << "##FORMAT=<ID=PL,Number=G,Type=Integer,Description=\"Normalized, Phred-scaled likelihoods for genotypes as defined in the VCF specification\">\n";
     _os << "##FORMAT=<ID=PR,Number=.,Type=Integer,Description=\"Spanning paired-read support for the ref and alt alleles in the order listed\">\n";
