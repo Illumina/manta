b44a696b427166d149731ab1f9696f9c9bb4d558
Chris Saunders <csaunders@illumina.com>
2016-01-08 09:44:56 -0800

    update source header copyright to 2016

--- src/c++/lib/blt_util/CircularCounter.hh
+++ src/c++/lib/blt_util/CircularCounter.hh
@@ -1,7 +1,7 @@
 // -*- mode: c++; indent-tabs-mode: nil; -*-
 //
 // Manta - Structural Variant and Indel Caller
-// Copyright (c) 2013-2015 Illumina, Inc.
+// Copyright (c) 2013-2016 Illumina, Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
