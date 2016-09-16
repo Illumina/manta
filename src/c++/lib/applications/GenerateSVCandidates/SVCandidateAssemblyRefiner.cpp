bbd79d4d479b3f21572f958d3f7082adc288eb85
Xiaoyu Chen <xchen@illumina.com>
2016-09-16 15:44:01 -0700

    add debugging info for the input ref seq of aligners

--- src/c++/lib/applications/GenerateSVCandidates/SVCandidateAssemblyRefiner.cpp
+++ src/c++/lib/applications/GenerateSVCandidates/SVCandidateAssemblyRefiner.cpp
@@ -1601,6 +1601,12 @@ getJumpAssembly(
         }
         else
         {
+#ifdef DEBUG_REFINER
+            log_os << __FUNCTION__ << " Ref1 for alignment: "
+            	   << bp1refSeq.substr(align1LeadingCut, bp1refSeq.size()-align1LeadingCut-align1TrailingCut) << '\n';
+            log_os << __FUNCTION__ << " Ref2 for alignment: "
+            	   << bp2refSeq.substr(align2LeadingCut, bp2refSeq.size()-align2LeadingCut-align2TrailingCut) << '\n';
+#endif
             _spanningAligner.align(contig.seq.begin(), contig.seq.end(),
                                    align1RefStrPtr->begin() + align1LeadingCut, align1RefStrPtr->end() - align1TrailingCut,
                                    align2RefStrPtr->begin() + align2LeadingCut, align2RefStrPtr->end() - align2TrailingCut,
@@ -1881,6 +1887,12 @@ getSmallSVAssembly(
             }
             adjustedTrailingCut=(refSize-(refIndex+merSize));
         }
+
+#ifdef DEBUG_REFINER
+            log_os << __FUNCTION__ << " Ref for alignment: "
+            	   << align1RefStr.substr(adjustedLeadingCut, align1RefStr.size()-adjustedLeadingCut-adjustedTrailingCut) << '\n';
+#endif
+            ;
 
         // start with largeSV aligner
         {
