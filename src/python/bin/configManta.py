24b3a30a83082de8d2d5ee43c3fe264beed8cc8f
Xiaoyu Chen <xchen@illumina.com>
2017-03-06 16:53:32 -0800

    update copyright year to 2017

--- src/python/bin/configManta.py
+++ src/python/bin/configManta.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # Manta - Structural Variant and Indel Caller
-# Copyright (c) 2013-2016 Illumina, Inc.
+# Copyright (c) 2013-2017 Illumina, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
